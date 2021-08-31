ff7a796e79b3337ad01d3ffd014e3098f4b83d94
GabrielEmer <gabriel_emer@hotmail.com>
2021-08-31 15:04:42 -0300

    Configurando o JsonIgnoreProperties nos Mixins das entidades.

---
+++ src/main/java/com/algaworks/algafood/core/jackson/JacksonMixinModule.java
@@ -0,0 +1,20 @@
+package com.algaworks.algafood.core.jackson;
+
+import com.algaworks.algafood.api.model.CidadeMixin;
+import com.algaworks.algafood.api.model.CozinhaMixin;
+import com.algaworks.algafood.api.model.RestauranteMixin;
+import com.algaworks.algafood.domain.model.Cidade;
+import com.algaworks.algafood.domain.model.Cozinha;
+import com.algaworks.algafood.domain.model.Restaurante;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JacksonMixinModule extends SimpleModule {
+
+    public JacksonMixinModule() {
+        setMixInAnnotation(Restaurante.class, RestauranteMixin.class);
+        setMixInAnnotation(Cozinha.class, CozinhaMixin.class);
+        setMixInAnnotation(Cidade.class, CidadeMixin.class);
+    }
+}
--- src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
+++ src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
@@ -1,7 +1,6 @@
 package com.algaworks.algafood.domain.model;
 
 import com.algaworks.algafood.core.validation.Groups;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import org.hibernate.annotations.CreationTimestamp;
@@ -29,19 +28,15 @@ public class Restaurante {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    //@NotNull
-    //@NotEmpty
     @NotBlank
     @Column(nullable = false)
     private String nome;
 
-    //@DecimalMin("0")
     @NotNull
     @PositiveOrZero
     @Column(name = "taxa_frete", nullable = false)
     private BigDecimal taxaFrete;
 
-    //	@JsonIgnore
     @Valid
     @ConvertGroup(from = Default.class, to = Groups.CozinhaId.class)
     @NotNull
@@ -49,29 +44,23 @@ public class Restaurante {
     @JoinColumn(name = "cozinha_id", nullable = false)
     private Cozinha cozinha;
 
-    @JsonIgnore
     @Embedded
     private Endereco endereco;
 
-    @JsonIgnore
     @CreationTimestamp
     @Column(nullable = false, columnDefinition = "datetime")
     private LocalDateTime dataCadastro;
 
-    @JsonIgnore
     @UpdateTimestamp
     @Column(nullable = false, columnDefinition = "datetime")
     private LocalDateTime dataAtualizacao;
 
-    @JsonIgnore
     @ManyToMany
     @JoinTable(name = "restaurante_forma_pagamento",
       joinColumns = @JoinColumn(name = "restaurante_id"),
       inverseJoinColumns = @JoinColumn(name = "forma_pagamento_id"))
     private List<FormaPagamento> formasPagamento = new ArrayList<>();
 
-    @JsonIgnore
     @OneToMany(mappedBy = "restaurante")
     private List<Produto> produtos = new ArrayList<>();
-
 }

0cc6f36c3715b303ddc99a9f6302b0c964f717c4
GabrielEmer <gabriel_emer@hotmail.com>
2021-09-04 10:45:21 -0300

    Correção de bug na exclusão;

--- src/main/java/com/algaworks/algafood/domain/service/CadastroCidadeService.java
+++ src/main/java/com/algaworks/algafood/domain/service/CadastroCidadeService.java
@@ -34,6 +34,7 @@ public class CadastroCidadeService {
     public void excluir(Long cidadeId) {
         try {
             cidadeRepository.deleteById(cidadeId);
+            cidadeRepository.flush();
         } catch (EmptyResultDataAccessException e){
             throw new CidadeNaoEncontradaException(cidadeId);
     } catch (DataIntegrityViolationException e) {

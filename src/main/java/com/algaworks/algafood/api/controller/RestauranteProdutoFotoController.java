600dc1c64a4cab8af8ab10b294d22d3cc4cadd71
GabrielEmer <gabriel_emer@hotmail.com>
2021-09-21 19:39:21 -0300

    Implementando o início do upload da foto do produto.

---
+++ src/main/java/com/algaworks/algafood/api/controller/RestauranteProdutoFotoController.java
@@ -0,0 +1,34 @@
+package com.algaworks.algafood.api.controller;
+
+import com.algaworks.algafood.api.model.input.ProdutoFotoInput;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.UUID;
+
+@RestController
+@RequestMapping(value = "/restaurantes/{restauranteId}/produtos/{produtoId}/foto")
+public class RestauranteProdutoFotoController {
+
+    @PutMapping
+    public void atualizar(@PathVariable Long restauranteId, @PathVariable Long produtoId,
+                          ProdutoFotoInput produtoFotoInput) {
+
+        System.out.println(produtoFotoInput.getArquivo().getContentType());
+        System.out.println(produtoFotoInput.getDescricao());
+
+        String newFileName = UUID.randomUUID().toString() + "_" + produtoFotoInput.getArquivo().getOriginalFilename();
+        try {
+            produtoFotoInput.getArquivo().transferTo(Paths.get("C:\\Users\\gabriel.emer\\Documents\\Produtos",
+                    newFileName));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}

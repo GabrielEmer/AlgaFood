2ca83b18f78261d81720fba31dfce0db3a92b0fe
GabrielEmer <gabriel_emer@hotmail.com>
2021-12-28 16:04:57 -0300

    Implementando cache na busca de formas de pagamento.

--- src/main/java/com/algaworks/algafood/api/controller/FormaPagamentoController.java
+++ src/main/java/com/algaworks/algafood/api/controller/FormaPagamentoController.java
@@ -8,11 +8,14 @@ import com.algaworks.algafood.domain.model.FormaPagamento;
 import com.algaworks.algafood.domain.repository.FormaPagamentoRepository;
 import com.algaworks.algafood.domain.service.CadastroFormaPagamentoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.CacheControl;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping("/formas-pagamento")
@@ -31,14 +34,18 @@ public class FormaPagamentoController {
     private FormaPagamentoInputDisassembler disassembler;
 
     @GetMapping
-    public List<FormaPagamentoModel> listar() {
-        return assembler.toCollectionModel(formaPagamentoRepository.findAll());
+    public ResponseEntity<List<FormaPagamentoModel>> listar() {
+        return ResponseEntity.ok()
+                .cacheControl(CacheControl.maxAge(10, TimeUnit.SECONDS).cachePublic())
+                .body(assembler.toCollectionModel(formaPagamentoRepository.findAll()));
     }
 
     @GetMapping
     @RequestMapping("/{formaPagamentoId}")
-    public FormaPagamentoModel buscar(@PathVariable Long formaPagamentoId) {
-        return assembler.toModel(cadastroFormaPagamento.buscar(formaPagamentoId));
+    public ResponseEntity<FormaPagamentoModel> buscar(@PathVariable Long formaPagamentoId) {
+        return ResponseEntity.ok()
+                .cacheControl(CacheControl.maxAge(10, TimeUnit.SECONDS).cachePublic())
+                .body(assembler.toModel(cadastroFormaPagamento.buscar(formaPagamentoId)));
     }
 
     @PostMapping

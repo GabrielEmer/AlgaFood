e2cf670f5d20544d3ec0ac04b8b2b73dd978c197
GabrielEmer <gabriel_emer@hotmail.com>
2021-12-28 16:16:51 -0300

    Adicionando campo de data da ultima atualização na tabela de forma de pagamento para implementação de deep etags.

--- src/main/java/com/algaworks/algafood/domain/repository/FormaPagamentoRepository.java
+++ src/main/java/com/algaworks/algafood/domain/repository/FormaPagamentoRepository.java
@@ -2,7 +2,14 @@ package com.algaworks.algafood.domain.repository;
 
 import com.algaworks.algafood.domain.model.FormaPagamento;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.time.OffsetDateTime;
+
 @Repository
-public interface FormaPagamentoRepository extends JpaRepository<FormaPagamento, Long> {}
+public interface FormaPagamentoRepository extends JpaRepository<FormaPagamento, Long> {
+
+    @Query("select max(data_atualizacao) from FormaPagamento")
+    OffsetDateTime getDataUltimaAtualizacao();
+}

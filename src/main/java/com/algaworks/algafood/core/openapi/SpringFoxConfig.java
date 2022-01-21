2fa6085120d64a07ab652e9921ae6e8435d8b54e
GabrielEmer <gabriel_emer@hotmail.com>
2022-01-21 11:48:53 -0300

    Adicionando uma tag para o controller de cidades.

--- src/main/java/com/algaworks/algafood/core/openapi/SpringFoxConfig.java
+++ src/main/java/com/algaworks/algafood/core/openapi/SpringFoxConfig.java
@@ -7,6 +7,7 @@ import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.Tag;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
@@ -20,8 +21,9 @@ public class SpringFoxConfig {
                     .apis(RequestHandlerSelectors.basePackage("com.algaworks.algafood.api"))
                     .paths(PathSelectors.any())
 //                  .paths(PathSelectors.ant("/restaurantes/*"))
-                .build()
-                .apiInfo(apiInfo());
+                    .build()
+                .apiInfo(apiInfo())
+                .tags(new Tag("Cidades", "Gerencia as cidades"));
     }
 
     public ApiInfo apiInfo() {

f3a7afca838113b29841715281adabbed2c3c260
GabrielEmer <gabriel_emer@hotmail.com>
2022-02-07 14:16:27 -0300

    Adicionando a descrição da tag de grupos

--- src/main/java/com/algaworks/algafood/core/openapi/SpringFoxConfig.java
+++ src/main/java/com/algaworks/algafood/core/openapi/SpringFoxConfig.java
@@ -44,7 +44,10 @@ public class SpringFoxConfig {
                 .globalResponses(HttpMethod.DELETE, globalDeleteResponseMessages())
                 .additionalModels(typeResolver.resolve(Problem.class))
                 .apiInfo(apiInfo())
-                .tags(new Tag("Cidades", "Gerencia as cidades"));
+                .tags(new Tag("Cidades", "Gerencia as cidades"))
+                .tags(new Tag("Grupos", "Gerencia os grupos de usuários"))
+
+                ;
     }
 
     private List<Response> globalDeleteResponseMessages() {
